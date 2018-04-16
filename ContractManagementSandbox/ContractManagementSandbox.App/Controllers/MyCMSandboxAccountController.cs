ec3ae4700066b74e13e874a659e82ee6dfd6bdea
Mattias-Lundh <mattiasanderslundh@gmail.com>
2018-04-16 12:31:00 +0200

    *a bit of bootsrap layout *added more controlers

---
+++ ContractManagementSandbox/ContractManagementSandbox.App/Controllers/MyCMSandboxAccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContractManagementSandbox.App.Controllers
+{
+    public class MyCMSandboxAccountController : Controller
+    {
+        // GET: MyCMSandboxAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
